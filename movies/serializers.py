cbff5923be4bdc21c734cc278f7ef43ee11aef22
Valente Alexsander <alevtelles@gmail.com>
2025-03-06 20:44:12 -0300

    Add validações

--- movies/serializers.py
+++ movies/serializers.py
@@ -2,8 +2,18 @@ from rest_framework import serializers
 from movies.models import Movie
 
 
-class MovieSerializer(serializers.ModelSerializer):
+class MovieModelSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise serializers.ValidationError("A data de lançamento deve ser superior a 1990")
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError("O resumo deve ter no máxmimo 200 caracteres")
+        return value
