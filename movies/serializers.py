7e0a856212dd5622ac562ca4c3ba9f50c9d276b0
Valente Alexsander <alevtelles@gmail.com>
2025-03-06 21:36:08 -0300

    Somando  valores de pontuação

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,16 +1,25 @@
+from django.db.models import Avg
+
 from rest_framework import serializers
 from movies.models import Movie
 
 
 class MovieModelSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
     
     class Meta:
         model = Movie
         fields = '__all__'
+    
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+        if rate:
+            return round(rate, 1)
+        return 0
 
     def validate_release_date(self, value):
-        if value.year < 1990:
-            raise serializers.ValidationError("A data de lançamento deve ser superior a 1990")
+        if value.year < 1900:
+            raise serializers.ValidationError("A data de lançamento deve ser superior a 1900")
         return value
     
     def validate_resume(self, value):
